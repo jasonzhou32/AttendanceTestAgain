58b53b52e3ceb524cf68981e1370e5ec9f4e130b
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-04-28 15:08:05 -0500

    Update session.php

--- Includes/session.php
+++ Includes/session.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start(); 
 
@@ -7,19 +6,5 @@ if (!isset($_SESSION['userId']))
   echo "<script type = \"text/javascript\">
   window.location = (\"../index.php\");
   </script>";
-
 }
-
-// $expiry = 1800 ;//session expiry required after 30 mins
-// if (isset($_SESSION['LAST']) && (time() - $_SESSION['LAST'] > $expiry)) {
-
-//     session_unset();
-//     session_destroy();
-//     echo "<script type = \"text/javascript\">
-//           window.location = (\"../index.php\");
-//           </script>";
-
-// }
-// $_SESSION['LAST'] = time();
-    
 ?>
