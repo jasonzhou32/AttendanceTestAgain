e96cda45051305e4c75713ad68d17be2454696bc
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-04-28 14:32:45 -0500

    Update dbcon.php

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -6,14 +6,17 @@
 	// $db = "attendancemsystem";
 
 	// Retrieve database connection details from environment variables
-	$host = mynewtestserver.mysql.database.azure.com;
-	$user = xilinnewtester;
-	$pass = MyTesterPass123;
-	$db = your_database_name;
-	$port = 3306;
+	// $host = mynewtestserver.mysql.database.azure.com;
+	// $user = xilinnewtester;
+	// $pass = MyTesterPass123;
+	// $db = your_database_name;
+	// $port = 3306;
 	
-	$conn = new mysqli($host, $user, $pass, $db, $port);
-	if($conn->connect_error){
-		echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
-	}
+	// $conn = new mysqli($host, $user, $pass, $db, $port);
+	// if($conn->connect_error){
+	// 	echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
+	// }
+
+	$con = mysqli_init();
+	mysqli_real_connect($conn, "mynewtestserver.mysql.database.azure.com", "xilinnewtester", "MyTesterPass123", "your_database_name", 3306);
 ?>
