14327520f050a6598d55162078d8bd707d4700c9
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-04-28 14:11:02 -0500

    Update dbcon.php

--- Includes/dbcon.php
+++ Includes/dbcon.php
@@ -1,11 +1,18 @@
 <?php
 	date_default_timezone_set('America/Chicago');
-	$host = "localhost";
-	$user = "root";
-	$pass = "";
-	$db = "attendancemsystem";
+	// $host = "localhost";
+	// $user = "root";
+	// $pass = "";
+	// $db = "attendancemsystem";
+
+	// Retrieve database connection details from environment variables
+	$host = getenv("DB_HOST");
+	$user = getenv("DB_USER");
+	$pass = getenv("DB_PASS");
+	$db = getenv("DB_NAME");
+	$port = getenv("DB_PORT");
 	
-	$conn = new mysqli($host, $user, $pass, $db);
+	$conn = new mysqli($host, $user, $pass, $db, $port);
 	if($conn->connect_error){
 		echo "Seems like you have not configured the database. Failed To Connect to database:" . $conn->connect_error;
 	}
