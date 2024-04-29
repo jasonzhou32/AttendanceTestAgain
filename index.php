9cf00e16b70f56caf4d1306d243e5b82f12d705d
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-04-28 23:21:29 -0500

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ include 'Includes/dbcon.php';
             justify-content: center;
             align-items: center;
             height: 100vh;
-            margin-bottom: 21.5%
+            margin-bottom: 21.5%;
         }
 
         .card {
