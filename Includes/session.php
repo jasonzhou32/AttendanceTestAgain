0b5684c6a92a0a85ad4468fa5973d0e8585145fa
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-04-28 19:21:04 -0500

    Update session.php

--- Includes/session.php
+++ Includes/session.php
@@ -2,10 +2,10 @@
 ob_start();
 session_start(); 
 
-// if (!isset($_SESSION['userId']))
-// {
-//   echo "<script type = \"text/javascript\">
-//   window.location = (\"../index.php\");
-//   </script>";
-// }
+if (!isset($_SESSION['userId']))
+{
+  echo "<script type = \"text/javascript\">
+  window.location = (\"../index.php\");
+  </script>";
+}
 ?>
