572113b8feff613bb532f6cd67adcf660f7149a1
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-05-12 19:08:39 -0500

    Update sidebar.php

--- ClassTeacher/Includes/sidebar.php
+++ ClassTeacher/Includes/sidebar.php
@@ -1,10 +1,3 @@
-<!-- Add a button to toggle the sidebar -->
-<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#sidebarCollapse" aria-controls="sidebarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="navbar-toggler-icon"></span>
-</button>
-
-<!-- Sidebar -->
-<div class="collapse navbar-collapse" id="sidebarCollapse">
 <ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
       <a class="sidebar-brand d-flex align-items-center bg-gradient-primary justify-content-center" href="index.php">
         <div class="sidebar-brand-icon" >
@@ -118,4 +111,3 @@
       <hr class="sidebar-divider">
      
     </ul>
-</div>
