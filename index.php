0a290724797eeee7e76eebefa69a933a46b72938
Jason Z <48233175+jasonzhou32@users.noreply.github.com>
2024-05-19 18:04:37 -0500

    Update index.php

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ include 'Includes/dbcon.php';
 
     <style>
         body {
-            background-image: url('img/logo/loral1.jpe00g');
+            background-image: url('img/logo/loral1.jpg');
             background-size: cover;
             background-repeat: no-repeat;
         }
@@ -31,33 +31,32 @@ include 'Includes/dbcon.php';
             display: flex;
             justify-content: center;
             align-items: center;
-            height: calc(100vh - 20%);
-            padding: 0 5%;
+            height: 100vh;
         }
 
         .card {
-            border-radius: 2%;
+            border-radius: 0.5rem;
             box-shadow: 0 0 1rem rgba(0, 0, 0, 0.1);
-            width: 100%;
+            width: 90%;
             max-width: 500px;
         }
 
         .login-form {
-            padding: 5%;
+            padding: 2rem;
         }
 
         .form-control {
-            border-radius: 2rem;
-            padding: 2%;
+            border-radius: 1rem;
+            padding: 0.75rem;
         }
 
         .btn-login {
-            border-radius: 2rem;
-            padding: 2%;
+            border-radius: 1rem;
+            padding: 0.75rem;
         }
 
         .alert {
-            border-radius: 1rem;
+            border-radius: 0.5rem;
         }
 
         /* Updated footer styling */
@@ -66,38 +65,32 @@ include 'Includes/dbcon.php';
             bottom: 0;
             width: 100%;
             background-color: #fff;
-            padding: 1%;
+            padding: 1rem 0;
             text-align: center;
-            border-top: 0.1rem solid #ddd;
+            border-top: 1px solid #ddd;
         }
 
-        @media (min-width: 768px) {
-            .container-login {
-                padding: 0 10%;
-            }
-
+        @media (min-width: 576px) {
             .card {
                 width: 80%;
             }
         }
 
-        @media (min-width: 992px) {
-            .container-login {
-                padding: 0 20%;
-            }
-
+        @media (min-width: 768px) {
             .card {
                 width: 60%;
             }
         }
 
-        @media (min-width: 1200px) {
-            .container-login {
-                padding: 0 30%;
-            }
-
+        @media (min-width: 992px) {
             .card {
-                width: 50%;
+                width: 40%;
+            }
+        }
+
+        @media (min-width: 1200px) {
+            .card {
+                width: 30%;
             }
         }
     </style>
@@ -105,43 +98,42 @@ include 'Includes/dbcon.php';
 
 <body class="bg-gradient-login">
     <div class="container-login">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card shadow-sm my-5">
-                <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="login-form">
-                                <h5 align="center">STUDENT ATTENDANCE SYSTEM</h5>
-                                <div class="text-center">
-                                    <img src="img/logo/attnlg.jpg" style="width:20%; height:auto;">
-                                    <br><br>
-                                    <h1 class="h4 text-gray-900 mb-4">Login Panel</h1>
+        <div class="card shadow-sm">
+            <div class="card-body p-0">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="login-form">
+                            <h5 align="center">STUDENT ATTENDANCE SYSTEM</h5>
+                            <div class="text-center">
+                                <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
+                                <br><br>
+                                <h1 class="h4 text-gray-900 mb-4">Login Panel</h1>
+                            </div>
+                            <form class="user" method="Post" action="">
+                                <div class="form-group">
+                                    <select required name="userType" class="form-control mb-3">
+                                        <option value="">--Select User Role--</option>
+                                        <option value="Administrator">Administrator</option>
+                                        <option value="ClassTeacher">Class Teacher</option>
+                                    </select>
                                 </div>
-                                <form class="user" method="Post" action="">
-                                    <div class="form-group">
-                                        <select required name="userType" class="form-control mb-3">
-                                            <option value="">--Select User Role--</option>
-                                            <option value="Administrator">Administrator</option>
-                                            <option value="ClassTeacher">Class Teacher</option>
-                                        </select>
+                                <div class="form-group">
+                                    <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
+                                </div>
+                                <div class="form-group">
+                                    <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
+                                </div>
+                                <div class="form-group">
+                                    <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck">
                                     </div>
-                                    <div class="form-group">
-                                        <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="password" required class="form-control" id="exampleInputPassword" placeholder="Enter Password">
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="custom-control custom-checkbox small" style="line-height: 1.5rem;">
-                                            <input type="checkbox" class="custom-control-input" id="customCheck">
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="submit" class="btn btn-success btn-block btn-login" value="Login" name="login" />
-                                    </div>
-                                </form>
+                                </div>
+                                <div class="form-group">
+                                    <input type="submit" class="btn btn-success btn-block btn-login" value="Login" name="login" />
+                                </div>
+                            </form>
 
-                                <?php
+                            <?php
 if(isset($_POST['login'])){
    $userType = $_POST['userType'];
    $username = $_POST['username'];
@@ -185,9 +177,8 @@ if(isset($_POST['login'])){
    }
 }
 ?>
-                                <div class="text-center">
-                                    <!-- Add social login buttons if required -->
-                                </div>
+                            <div class="text-center">
+                                <!-- Add social login buttons if required -->
                             </div>
                         </div>
                     </div>
